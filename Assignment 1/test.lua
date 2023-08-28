3333d2a683f0cd61f1ef02f621ca1a131c1e0c4b
Shubham <sbdaule5@gmail.com>
2023-08-29 01:10:03 +0530

    Complete

--- Assignment 1/test.lua
+++ Assignment 1/test.lua
@@ -1,27 +1,56 @@
--- printing values of arrays
---[[ This is a long commit
-  It can span multiple lines
-  ]]
-
-local str1 = "This is a lua string"
-local str2 = "This is a multiline lua \
-string that works by escaping the newline character"
--- local str3 = " But this string does not escape
--- newline character so this is not valid"
-local str4 = "Hello this is a string!"
-local str5 = [[
-   This is a proper multiline string
-   We are free to write any thing here
-   No need to escape newlines :)
-]]
-     -- 3.0     3.1416     314.16e-2     0.31416E1     34e1
-     -- 0x0.1E  0xA23p-4   0X1.921FB54442D18P+1
-local float1 = 5.5
-local float2 = 5.
-local float3 = .5
--- local float4 = .  -- not valid
-
-local arr={2,3,2345,264236,23452345,2456, .5, 5., -.5, 1e-1, 0xa.}
-for i=1, #arr do
-   print(arr[i])
-end
+-- Markov Chain Program in Lua
+    
+    function allwords ()
+      local line = io.read()    -- current line
+      local pos = 1             -- current position in the line
+      return function ()        -- iterator function
+        while line do           -- repeat while there are lines
+          local s, e = string.find(line, "%w+", pos)
+          if s then      -- found a word?
+            pos = e + 1  -- update next position
+            return string.sub(line, s, e)   -- return the word
+          else
+            line = io.read()    -- word not found; try next line
+            pos = 1             -- restart from first position
+          end
+        end
+        return nil            -- no more lines: end of traversal
+      end
+    end
+    
+    function prefix (w1, w2)
+      return w1 .. ' ' .. w2
+    end
+    
+    local statetab
+    
+    function insert (index, value)
+      if not statetab[index] then
+        statetab[index] = {n=0}
+      end
+      table.insert(statetab[index], value)
+    end
+    
+    local N  = 2
+    local MAXGEN = 10000
+    local NOWORD = "\n"
+    
+    -- build table
+    statetab = {}
+    local w1, w2 = NOWORD, NOWORD
+    for w in allwords() do
+      insert(prefix(w1, w2), w)
+      w1 = w2; w2 = w;
+    end
+    insert(prefix(w1, w2), NOWORD)
+    -- generate text
+    w1 = NOWORD; w2 = NOWORD     -- reinitialize
+    for i=1,MAXGEN do
+      local list = statetab[prefix(w1, w2)]
+      -- choose a random item from list
+      local r = math.random(table.getn(list))
+      local nextword = list[r]
+      if nextword == NOWORD then return end
+      io.write(nextword, " ")
+      w1 = w2; w2 = nextword
+    end
